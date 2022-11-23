cb35d0b98cac9456a72411afcf5402f97dceb6aa
Anish <acharyaanish920@gmail.com>
2022-11-23 14:57:18 +0545

    responsive ul color correction

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -67,11 +67,9 @@ export default function Navbar(props) {
                   : "") || (scroll === true && apply === false ? "toggle " : "")
               }`}
               id="ul"
-              style={
-                {
-                  // backgroundColor: `${props.mode === "black" ? "#2e2e2e" : ""}`,
-                }
-              }
+              style={{
+                backgroundColor: `${props.mode === "black" ? "#2e2e2e" : ""}`,
+              }}
             >
               {Links.map((link) => (
                 <Link
