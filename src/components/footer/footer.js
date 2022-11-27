9cbfae63ec4c751132f021beeab1514d3a2a0f76
sarad096 <saradgyawali096@gmail.com>
2022-11-27 17:40:10 +0545

    refactored code

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -10,7 +10,7 @@ export default function Footer(props) {
         <div>
           <div className="footer-icons">
             {Icons.map((icon, id) => (
-              <a href={`${icon.link}`} key={`${icon.id}`} target="_blank">
+              <a href={`${icon.link}`} key={`${icon.id}`} target="_blank" rel="noreferrer">
                 <Icon
                   icon={`${icon.svg}`}
                   color={props.mode === "light" ? "black" : "white"}
